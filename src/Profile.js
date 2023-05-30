55fd6e764bbc2a3166f317fcf250c58dc1d5f95c
Atal Negi <93611928+atalnegi@users.noreply.github.com>
2023-05-30 21:46:30 +0530

    Update Profile.js
    using pinata api because infura gateway is not public anymore

--- src/Profile.js
+++ src/Profile.js
@@ -1,9 +1,6 @@
 import { useState, useEffect } from 'react'
-import { ethers } from "ethers"
-import { Row, Form, Button, Card, ListGroup, Col } from 'react-bootstrap'
-import { create as ipfsHttpClient } from 'ipfs-http-client'
-const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
-
+import { Row, Form, Button, Card, Col } from 'react-bootstrap'
+import axios from 'axios';
 
 const App = ({ contract }) => {
     const [profile, setProfile] = useState('')
@@ -19,8 +16,11 @@ const App = ({ contract }) => {
             // get uri url of nft
             const uri = await contract.tokenURI(i)
             // fetch nft metadata
+            console.log("URI",uri)
             const response = await fetch(uri)
+            console.log("Response",response)
             const metadata = await response.json()
+            console.log("MEta",metadata)
             return ({
                 id: i,
                 username: metadata.username,
@@ -37,29 +37,82 @@ const App = ({ contract }) => {
         setProfile(profile)
         setLoading(false)
     }
-    const uploadToIPFS = async (event) => {
-        event.preventDefault()
-        const file = event.target.files[0]
-        if (typeof file !== 'undefined') {
-            try {
-                const result = await client.add(file)
-                setAvatar(`https://ipfs.infura.io/ipfs/${result.path}`)
-            } catch (error) {
-                console.log("ipfs image upload error: ", error)
-            }
+
+    const sendJSONtoIPFS = async (ImgHash) => {
+
+        try {
+    
+          const resJSON = await axios({
+            method: "post",
+            url: "https://api.pinata.cloud/pinning/pinJsonToIPFS",
+            data: {
+              "username": username,
+              "avatar": ImgHash
+            },
+            headers: {
+                'pinata_api_key': process.env.REACT_APP_PINATA_API_KEY,
+                'pinata_secret_api_key': process.env.REACT_APP_PINATA_SECRET_API_KEY,
+    
+            },
+          });    
+    
+          const tokenURI = `https://gateway.pinata.cloud/ipfs/${resJSON.data.IpfsHash}`;
+          console.log("Token URI", tokenURI);
+
+          mintProfile(tokenURI)
+        } catch (error) {
+            window.alert("JASON to IPFS error: ", error)
         }
+    
+    
     }
-    const mintProfile = async (event) => {
+
+    const uploadToIPFS = async (e) => {
+
+        e.preventDefault();
+        console.log("123");
+        console.log(e);
+        if (avatar) {
+          try {
+    
+            console.log("1234");
+            const formData = new FormData();
+            formData.append("file", avatar);
+            console.log(formData)
+            const resFile = await axios({
+                method: "post",
+                url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+                data: formData,
+                headers: {
+                    'pinata_api_key': process.env.REACT_APP_PINATA_API_KEY,
+                    'pinata_secret_api_key': process.env.REACT_APP_PINATA_SECRET_API_KEY,
+                    "Content-Type": "multipart/form-data"
+                },
+              });
+    
+            const ImgHash = `https://gateway.pinata.cloud/ipfs/${resFile.data.IpfsHash}`;
+            console.log(ImgHash);
+            sendJSONtoIPFS(ImgHash)
+    
+    
+          } catch (error) {
+            console.log("File to IPFS: ")
+            console.log(error)
+          }
+        }
+      } 
+    
+      const mintProfile = async (uri) => {
         if (!avatar || !username) return
         try {
-            const result = await client.add(JSON.stringify({ avatar, username }))
             setLoading(true)
-            await (await contract.mint(`https://ipfs.infura.io/ipfs/${result.path}`)).wait()
+            await (await contract.mint(uri)).wait()
             loadMyNFTs()
         } catch (error) {
             window.alert("ipfs uri upload error: ", error)
         }
-    }
+    }  
+    
     const switchProfile = async (nft) => {
         setLoading(true)
         await (await contract.setProfile(nft.id)).wait()
@@ -82,22 +135,17 @@ const App = ({ contract }) => {
             {profile ? (<div className="mb-3"><h3 className="mb-3">{profile.username}</h3>
                 <img className="mb-3" style={{ width: '400px' }} src={profile.avatar} /></div>)
                 :
-                <h4 className="mb-4">No NFT profile, please create one...</h4>}
+                <h4 className="mb-4">No profile, please create one...</h4>}
 
             <div className="row">
                 <main role="main" className="col-lg-12 mx-auto" style={{ maxWidth: '1000px' }}>
                     <div className="content mx-auto">
                         <Row className="g-4">
-                            <Form.Control
-                                type="file"
-                                required
-                                name="file"
-                                onChange={uploadToIPFS}
-                            />
+                        <Form.Control onChange={(e) => setAvatar(e.target.files[0])} size="lg" required type="file" name="file" />
                             <Form.Control onChange={(e) => setUsername(e.target.value)} size="lg" required type="text" placeholder="Username" />
                             <div className="d-grid px-0">
-                                <Button onClick={mintProfile} variant="primary" size="lg">
-                                    Mint NFT Profile
+                                <Button onClick={uploadToIPFS} variant="primary" size="lg">
+                                    Create Profile
                                 </Button>
                             </div>
                         </Row>
